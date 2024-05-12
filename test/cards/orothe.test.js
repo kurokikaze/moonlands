6df295860125440d988476b09294bcaddc60e4aa
Shirokov <sergei.shirokov@t-systems.com>
2024-05-12 17:16:32 +0200

    Removed console.dir from the Orothe test

--- test/cards/orothe.test.js
+++ test/cards/orothe.test.js
@@ -1786,7 +1786,6 @@ describe('Abaquist', () => {
     expect(gameState.state.promptParams.restrictions[0].type).toEqual(RESTRICTION_ENERGY_LESS_THAN);
     expect(gameState.state.promptParams.restrictions[0].value).toEqual(6);
 
-    console.dir(seenActions);
     expect(seenActions[2].restrictions[0].type).toEqual(RESTRICTION_ENERGY_LESS_THAN);
     expect(seenActions[2].restrictions[0].value).toEqual(6);
 
