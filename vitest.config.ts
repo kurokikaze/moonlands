63f055da482f807484a3a49f35fef851bdafc6ef
Sergei Shirokov <bolter.fire@gmail.com>
2026-09-05 13:27:01 +0200

    Automated unmaker tests by wrapping normal card tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
             'test/**/*.{test,spec}.{js,ts}',
             'src/**/*.{test,spec}.{js,ts}',
         ],
-        exclude: ['dist/**', 'node_modules/**'],
+        exclude: [
+            'dist/**',
+            'node_modules/**',
+            'test/cards/arderial.test.js',
+        ],
         coverage: {
             provider: 'v8',
             reporter: ['text', 'lcov'],
